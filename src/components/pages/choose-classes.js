bbdd8d9dcc6381aaf4ecc82111dde4a5d29552c7
James Madden <jamesmadden@James-MacBook-Air.local>
2019-02-08 21:03:14 -0800

    Add choose classes page

---
+++ src/components/pages/choose-classes.js
@@ -0,0 +1,12 @@
+import { html } from 'lit-element';
+import { Page } from '../page';
+
+export class ChooseClasses extends Page {
+
+  render () {
+    return html`<p>Coming Soon!</p>`;
+  }
+
+}
+
+customElements.define('choose-classes-page', ChooseClasses);
