07b5b2b174dccd03a2b40418599a01a6f6ccce6a
James Madden <jamesmadden@James-MacBook-Air.local>
2019-02-10 12:08:12 -0800

    Change h2 to p

--- src/components/pages/select-classes.js
+++ src/components/pages/select-classes.js
@@ -7,37 +7,62 @@ export class SelectClasses extends Page {
     return html`
       <style>
         ${this.pageStyles}
-        h2[slot=title] {
+        p[slot=title] {
           margin-top: 0px;
           margin-bottom: 0px;
         }
       </style>
       <page-header title="Select Classes" .tabs=${this.tabs}></page-header>
-      <main class="tab">
+      <main class="tab scrollable">
         <graviton-panel>
-          <h2 slot="title">1.1</h2>
+          <p slot="title">1.1</p>
           <div slot="body">
             <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
           </div>
         </graviton-panel>
         <graviton-panel>
-          <h2 slot="title">1.2</h2>
+          <p slot="title">1.2</p>
           <div slot="body">
             <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
           </div>
         </graviton-panel>
         <graviton-panel>
-          <h2 slot="title">1.3</h2>
+          <p slot="title">1.3</p>
           <div slot="body">
             <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
           </div>
         </graviton-panel>
         <graviton-panel>
-          <h2 slot="title">1.4</h2>
+          <p slot="title">1.4</p>
           <div slot="body">
             <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
           </div>
         </graviton-panel>
+        <graviton-panel>
+          <p slot="title">2.1</p>
+          <div slot="body">
+            <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
+          </div>
+        </graviton-panel>
+        <graviton-panel>
+          <p slot="title">2.2</p>
+          <div slot="body">
+            <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
+          </div>
+        </graviton-panel>
+        <graviton-panel>
+          <p slot="title">2.3</p>
+          <div slot="body">
+            <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
+          </div>
+        </graviton-panel>
+        <graviton-panel>
+          <p slot="title">2.4</p>
+          <div slot="body">
+            <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
+          </div>
+        </graviton-panel>
+        <graviton-button filled>Begin</graviton-button>
       </main>
     `;
   }
