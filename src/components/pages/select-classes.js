5bfefe5beb1f68ea24091debe6a61e3a8f5384f0
James Madden <jamesmadden@James-MacBook-Air.local>
2019-02-10 11:21:43 -0800

    Add Test Expansion Panels

--- src/components/pages/select-classes.js
+++ src/components/pages/select-classes.js
@@ -7,10 +7,38 @@ export class SelectClasses extends Page {
     return html`
       <style>
         ${this.pageStyles}
+        h2[slot=title] {
+          margin-top: 0px;
+          margin-bottom: 0px;
+          height: 32px;
+        }
       </style>
       <page-header title="Select Classes" .tabs=${this.tabs}></page-header>
       <main class="tab">
-        <p>Coming Soon</p>
+        <graviton-panel>
+          <h2 slot="title">1.1</h2>
+          <div slot="body">
+            <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
+          </div>
+        </graviton-panel>
+        <graviton-panel>
+          <h2 slot="title">1.2</h2>
+          <div slot="body">
+            <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
+          </div>
+        </graviton-panel>
+        <graviton-panel>
+          <h2 slot="title">1.3</h2>
+          <div slot="body">
+            <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
+          </div>
+        </graviton-panel>
+        <graviton-panel>
+          <h2 slot="title">1.4</h2>
+          <div slot="body">
+            <p>Graviton Panels allow for expanding cards, much like material-expansionpanel in the old version.</p>
+          </div>
+        </graviton-panel>
       </main>
     `;
   }
