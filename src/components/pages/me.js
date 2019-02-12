7d7b92f99fb05a2a681cb8912bea260be34b704b
James Madden <jamesmadden@James-MacBook-Air.local>
2019-02-12 15:44:28 -0800

    Increase Version Number

--- src/components/pages/me.js
+++ src/components/pages/me.js
@@ -78,7 +78,7 @@ export class MePage extends Page {
           </graviton-card>
           <graviton-card>
               <h3>App Version</h3>
-              <h2>Version 10.0.0</h2>
+              <h2>Version 10.0.1</h2>
           </graviton-card>
         </main>
       </tab-view>
