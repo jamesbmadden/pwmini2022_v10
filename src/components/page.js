d8eb36dc0b7ce707bd205ef0907a4a2bbe529ec1
James Madden <jamesbmadden@gmail.com>
2019-02-18 18:37:33 -0800

    Import header only

--- src/components/page.js
+++ src/components/page.js
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 import { LitElement, customElement } from 'lit-element';
-import { HeaderComponent, TabContainer, TabComponent, TabView } from './tabs';
+import { HeaderComponent } from './tabs/header';
 import { BottomAppBar } from './bottom-app-bar/bottom-app-bar';
 import pageStyles from './page.less';
 
