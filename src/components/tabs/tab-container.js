d812a3f7b3ae0dcb4b66813b910602d8351500ea
James Madden <jamesbmadden@gmail.com>
2019-02-18 18:38:24 -0800

    Move TabContainer to its own file

---
+++ src/components/tabs/tab-container.js
@@ -0,0 +1,81 @@
+import { LitElement, html } from 'lit-element';
+
+export class TabContainer extends LitElement {
+  static get properties () {
+    return {
+      tabs: { type: Array },
+      selected: { type: Number },
+      id: { type: String },
+      light: { type: Boolean }
+    }
+  }
+  constructor () {
+    super();
+    this.selected = 0;
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    document.dispatchEvent(new CustomEvent(`tabs-created-${this.id}`, { detail: { element: this } }));
+  }
+  render () {
+    return this.tabs.length > 0 ? html`
+      <style>
+        nav {
+          position:relative;
+          width:100%;
+          height:3.5rem;
+          display:flex;
+          flex-direction:row;
+        }
+        .header-tabindicator {
+          position:absolute;
+          left:0px;
+          top:52px;
+          height:8px;
+          width:${100/this.tabs.length}%;
+          border-radius:4px;
+          background-color:${red};
+          z-index:1;
+          transition:transform 0.4s cubic-bezier(0.4,0,0,1);
+          transform:translate(${100*this.selected}%);
+        }
+        .header-tabindicator[light] {
+          background-color: white;
+        }
+        tab-component {
+          width:100%;
+        }
+      </style>
+      <nav>
+        ${this.tabs.map((tab, index) => html`<tab-component ?light=${this.light} .selected=${this.selected == index} .index=${index} .tabChange=${tab => {
+          this.selected = tab;
+          this.dispatchEvent(new CustomEvent('tab-change', { detail: {tab:tab}}));
+        }}>${tab}</tab-component>`)}
+        <span class="header-tabindicator" ?light=${this.light}></span>
+      </nav>` : null;
+  }
+  touchStart() {
+    this.shadowRoot.querySelector('.header-tabindicator').style.transition = 'none';
+  }
+  touchMove(x) {
+    this.shadowRoot.querySelector('.header-tabindicator').style.transform = `translate(calc(${100*this.selected}% - ${x/this.tabs.length}px))`;
+  }
+  touchEnd(tab) {
+    this.selected = tab;
+    this.dispatchEvent(new CustomEvent('tab-change', { detail: {tab:tab}}));
+    this.shadowRoot.querySelector('.header-tabindicator').style.transition = 'transform 0.4s cubic-bezier(0,0,0,1)';
+    this.shadowRoot.querySelector('.header-tabindicator').style.transform = `translate(${100*this.selected}%)`;
+    setTimeout(() => {
+      this.shadowRoot.querySelector('.header-tabindicator').style.transition = null;
+      this.shadowRoot.querySelector('.header-tabindicator').style.transform = null;
+    }, 400);
+  }
+  touchCancel() {
+    this.shadowRoot.querySelector('.header-tabindicator').style.transition = 'transform 0.4s cubic-bezier(0,0,0,1)';
+    this.shadowRoot.querySelector('.header-tabindicator').style.transform = `translate(${100*this.selected}%)`;
+    setTimeout(() => {
+      this.shadowRoot.querySelector('.header-tabindicator').style.transition = null;
+      this.shadowRoot.querySelector('.header-tabindicator').style.transform = null;
+    }, 400);
+  }
+}
