8d325f7baaa2acf14ba3de34a49493bd79d1820e
James Madden <jamesbmadden@gmail.com>
2019-02-18 19:28:09 -0800

    Move gvt-button styles to its own file

--- src/components/graviton/graviton-button.js
+++ src/components/graviton/graviton-button.js
@@ -1,4 +1,5 @@
 import { LitElement, html } from 'lit-element';
+import gravitonButtonStyles from 'graviton-button.less';
 
 export class GravitonButton extends LitElement {
   static get properties() {
@@ -8,38 +9,7 @@ export class GravitonButton extends LitElement {
   }
   render() {
     return html`
-      <style>
-        .btn {
-          box-sizing:border-box;
-          display:inline-block;
-          background-color:#fafafa;
-          min-width:4rem;
-          height:2.25rem;
-          padding:0rem 1rem;
-          border:0px;
-          color:#222;
-          border-radius:1.125rem;
-          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
-          cursor:pointer;
-          overflow:hidden;
-          font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
-          outline: none;
-          -webkit-touch-callout: none; /* iOS Safari */
-          -webkit-user-select: none; /* Safari */
-          -moz-user-select: none; /* Firefox */
-          -ms-user-select: none; /* Internet Explorer/Edge */
-          user-select: none;
-        }
-        .btn.fill {
-          color: white;
-          background:${grad};
-          box-shadow: 0px 3px 6px rgba(244, 67, 54, 0.16);
-        }
-        .btn-wrapping {
-          display:inline-block;
-          margin:0.25rem 0rem;
-        }
-      </style>
+      <style>${gravitonButtonStyles.toString()}</style>
       <div class="btn-wrapping">
         <button class="btn ${this.filled ? 'fill' : 'light'}">
           <slot></slot>
