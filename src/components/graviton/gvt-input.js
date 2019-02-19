8af7dfacf099632b4892081f7c7f0281ca0112a3
James Madden <jamesbmadden@gmail.com>
2019-02-18 19:44:02 -0800

    Move styles to less file

--- src/components/graviton/gvt-input.js
+++ src/components/graviton/gvt-input.js
@@ -1,4 +1,5 @@
 import { LitElement, html } from 'lit-element';
+import gvtInputStyles from './gvt-input.less';
 
 export class GravitonInput extends LitElement {
   static get properties() {
@@ -16,59 +17,7 @@ export class GravitonInput extends LitElement {
   }
   render() {
     return html`
-      <style>
-        * {
-          font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
-        }
-        .fieldInput {
-          box-sizing:border-box;
-          padding:0px 12px;
-          width:100%;
-          height:3.5rem;
-          background:#fff;
-          border:1px solid #bdbdbd;
-          border-radius: 4px;
-          position:relative;
-          font-size:16pt;
-          z-index:1;
-          -webkit-appearance: none;
-          transition:border 0.2s cubic-bezier(1,0,0,1);
-        }
-        .fieldInputLabel {
-          z-index:2;
-          position:absolute;
-          top:18px;
-          left:2px;
-          color:#222;
-          background: #fff;
-          transition:transform 0.2s cubic-bezier(1,0,0,1), font-size 0.2s cubic-bezier(1,0,0,1), color 0.2s cubic-bezier(1,0,0,1);
-          transform:translate(4px,-24px);
-          font-size:12px;
-          pointer-events:none;
-        }
-        supports:placeholder-shown,.fieldInputLabel {
-          transform:translate(0px,0px);
-          font-size:16px;
-        }
-        .fieldInput:focus {
-          outline: none;
-          border: 1px solid ${red};
-        }
-        .fieldInput:focus ~ .highlight {
-          background-color:${red};
-        }
-        .fieldInput:focus ~ .fieldInputLabel {
-          color:${red};
-        }
-        .fieldInput:focus ~ .fieldInputLabel, .fieldInput:not(:placeholder-shown) ~ .fieldInputLabel {
-          transform:translate(4px,-24px);
-          font-size:12px;
-        }
-        .inputContainer {
-          position:relative;
-          margin-top:12px;
-        }
-      </style>
+      <style>${gvtInputStyles.toString()}</style>
       <div class="inputContainer">
         <input .value=${this.value} @input=${(e)=> {
           this.value = e.target.value;
