7d945863166cc602a9d07dd5ad38bc448be9988d
James Madden <jamesbmadden@gmail.com>
2019-03-13 20:15:05 -0700

    Set Fab Icon to SVG

--- src/components/pages/classes.js
+++ src/components/pages/classes.js
@@ -311,7 +311,7 @@ export class ClassesPage extends Page {
         this.dialogueOpen = true;
         this.uploadFileUri = false;
       }}>
-        <i class="material-icons">add</i>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
         <mwc-ripple accent></mwc-ripple>
       </div>
     `;
--- src/components/pages/mini.js
+++ src/components/pages/mini.js
@@ -57,7 +57,7 @@ export class MiniPage extends Page {
         })}` : html`<p>Nothing Yet!</p>`}
       </main>
       <div class="fab" disabled>
-        <i class="material-icons">add</i>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
       </div>
     `;
   }
