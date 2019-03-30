65962e811b6a1256d82640e7c975d408763b43c0
James Madden <jamesbmadden@gmail.com>
2019-03-29 17:35:26 -0700

    Service Worker now Ignores requests other than GET

--- dist/sw.js
+++ dist/sw.js
@@ -27,19 +27,21 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-  if (/powmini2022.firebaseapp.com\/api/.test(event.request.url)) {
-    addToCache(event.request, 'mini22-api');
-    event.respondWith(fromNetwork(event.request, 'mini22-api').then(response => {
-      return response;
-    }).catch(() => {
-      return fromCache(event.request, 'mini22-api');
-    }));
-  } else if (!/firestore.googleapis.com/.test(event.request.url)) {
-    addToCache(event.request, 'mini22-10');
-    event.respondWith(fromCache(event.request, 'mini22-10').then(response => {
-      if (response) return response; // fromCache won't error, so whether response exists needs to be checked
-      else return fromNetwork(event.request, 'mini22-10'); // If the file is not cached, get it from the network
-    }));
+  if (event.request.method === 'GET') {
+    if (/powmini2022.firebaseapp.com\/api/.test(event.request.url)) {
+      addToCache(event.request, 'mini22-api');
+      event.respondWith(fromNetwork(event.request, 'mini22-api').then(response => {
+        return response;
+      }).catch(() => {
+        return fromCache(event.request, 'mini22-api');
+      }));
+    } else if (!/firestore.googleapis.com/.test(event.request.url)) {
+      addToCache(event.request, 'mini22-10');
+      event.respondWith(fromCache(event.request, 'mini22-10').then(response => {
+        if (response) return response; // fromCache won't error, so whether response exists needs to be checked
+        else return fromNetwork(event.request, 'mini22-10'); // If the file is not cached, get it from the network
+      }));
+    }
   }
 });
 
