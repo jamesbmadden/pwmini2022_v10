d191459f16f88a70e26dbbc9fd4bf328e6c245a3
James Madden <jamesbmadden@gmail.com>
2019-06-21 15:44:59 -0700

    add options dialogue

--- src/components/pages/classes.js
+++ src/components/pages/classes.js
@@ -32,12 +32,18 @@ export class ClassesPage extends Page {
       uploadTitle: { type: String },
       uploadError: { type: String },
       dialogueLoading: { type: Boolean },
-      uploadIsHomework: { type: Number }
+      uploadIsHomework: { type: Number },
+      optionsOpen: { type: Boolean },
+      options: { type: Object },
+      rerender: { type: Number }
     }
   }
   constructor () {
     super();
     this.dialogueOpen = location.pathname.split('/')[2] === 'add';
+    this.rerender = 0;
+    this.options = JSON.parse(localStorage.getItem('config'));
+    this.serviceWorker = 'serviceWorker' in navigator;
     if (this.mini == undefined) {
       this.mini = [];
     }
@@ -88,6 +94,9 @@ export class ClassesPage extends Page {
     }
   }
   render () {
+    const classes = blocks.map(block => {
+      return this.user ? this.user.classes[block] : 'loading...';
+    });
     const homework = this.user.homework.flat().map(val => {
       let work = val;
       work.colour = '#2196f3';
@@ -148,7 +157,9 @@ export class ClassesPage extends Page {
         }
       </style>
       <page-header title="Upcoming" .tabs=${this.tabs}></page-header>
-      <img class="settings-icon" src='data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path fill="white" d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>' />
+      <img @click=${() => {
+        this.optionsOpen = true;
+      }} class="settings-icon" src='data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path fill="white" d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>' />
       <tab-view for="Upcoming-tabs">
         <main class="tab scrollable">
           <pwm-upcoming .events=${calendar}></pwm-upcoming>
@@ -321,6 +332,74 @@ export class ClassesPage extends Page {
         }}>Post</gvt-button><gvt-button @click=${() => {
           history.back();
         }}>Close</gvt-button></div>`}
+      </app-dialogue>
+      <app-dialogue ?open=${this.optionsOpen}>
+        <tab-view slot="body" for="settings-dialogue-tabs">
+          <style>
+            .option-supported {
+              color: #7cb342;
+            }
+            .option-unsupported {
+              color: #e53935;
+            }
+          </style>
+          <main>
+            <h2>Account Options</h2>
+            <enter-fade>
+              <h3>Account</h3>
+              <h2>${this.userData.email}</h2>
+              <gvt-button filled @click=${this.signOut}>Sign Out</gvt-button>
+            </enter-fade>
+            <enter-fade>
+              <h3>Classes</h3>
+              <ul>
+                ${classes.map(theClass => {
+                  return html`<li>${theClass}</li>`;
+                })}
+              </ul>
+              <gvt-button filled @click=${() => {
+                document.dispatchEvent(new CustomEvent('set-page', { detail: { page: 'select-classes'} }));
+              }}>Change</gvt-button>
+            </enter-fade>
+          </main>
+          <main>
+            <h2>App Options</h2>
+            <enter-fade>
+              <h3>Images</h3>
+              <h2 class="option-${this.options.images ? 'supported' : 'unsupported'}">${this.options.images ? 'Enabled' : 'Disabled'}</h2>
+              <gvt-button filled @click=${() => {
+                this.options.images = !this.options.images;
+                this.rerender++;
+                localStorage.setItem('config', JSON.stringify(this.options));
+              }}>${this.options.images ? 'Disable' : 'Enable'}</gvt-button>
+            </enter-fade>
+            <enter-fade>
+              <h3>Offline</h3>
+              ${this.serviceWorker ? html`<h2 class="option-supported">Supported</h2>` : html`<h2 class="option-unsupported">Unsupported In Your Browser</h2>`}
+            </enter-fade>
+            <enter-fade>
+                <h3>App Version</h3>
+                <h2>Version 10.3.2</h2>
+            </enter-fade>
+            <details>
+              <summary><h3 style="display:inline-block;cursor:pointer">Debug</h3></summary>
+              <enter-fade>
+                <h3>Stored Data</h3>
+                <gvt-button filled @click=${async () => {
+                  await Promise.all([
+                    caches.delete('mini22-10'),
+                    caches.delete('mini22-api')
+                  ])
+                }}>Clear Cache</gvt-button>
+                <gvt-button @click=${() => localStorage.clear()}>Clear LocalStorage</gvt-button>
+              </enter-fade>
+            </details>
+          </main>
+        </tab-view>
+        <tab-container slot="header" id="settings-dialogue-tabs" .tabs=${['Account', 'App']} .selected=${0}></tab-container>
+        <div slot="footer">
+          <gvt-button>Close</gvt-button>
+        </div>
       </app-dialogue>
       <div class="fab" @click=${() => {
         history.pushState({ page: 'classes', state: 'add' }, 'Classes: Add', '/classes/add');
