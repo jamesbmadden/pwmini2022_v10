265646f30f0c95a2d205564e83718429cf55b11d
James Madden <jamesbmadden@gmail.com>
2019-07-06 11:47:54 -0700

    Add polyfill for web components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
 import { LitElement, html, customElement, property } from 'lit-element';
 import '@graviton/button';
 import '@graviton/input';
