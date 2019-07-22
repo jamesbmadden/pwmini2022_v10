7269045754ec7e4b338a2324e5d7951c6fabaad8
James Madden <jamesbmadden@gmail.com>
2019-07-21 21:34:40 -0700

    Simple POST mixed-content interface

--- functions/src/upload.js
+++ functions/src/upload.js
@@ -1,3 +1,47 @@
+const { parse } = require('querystring');
+const inspect = require('util').inspect;
+
 const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
+
+const Busboy = require('busboy');
+
+const responseHeaders = {'Access-Control-Allow-Origin': '*', 'PW-Mini-Version': '10.5.0', 'content-type':'application/json'};
+
+function buildBody (request) {
+  return new Promise((resolve, reject) => {
+    const body = {};
+    const busboy = new Busboy({headers: request.headers});
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+      console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+      file.on('data', function(data) {
+        body[fieldname] = data;
+        console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+      });
+      file.on('end', function() {
+        console.log('File [' + fieldname + '] Finished');
+      });
+    });
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+      body[fieldname] = val;
+    });
+    busboy.on('finish', () => {
+      console.log('busboy finished');
+      resolve(body);
+    })
+    busboy.end(request.rawBody);
+  });
+}
+
+module.exports = functions.https.onRequest(async (request, response) => {
+  if (request.method === 'POST') {
+    const body = await buildBody(request);
+    response.writeHead(200, responseHeaders);
+    response.end(JSON.stringify(body));
+  } else {
+    response.writeHead(405, responseHeaders);
+    response.end(JSON.stringify({success: false, error: 'POST request must be used', method: request.method}));
+  }
+});
